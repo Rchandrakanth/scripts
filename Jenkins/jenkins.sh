b9f0ee799aaf4a17287b5e96974aabef19190c8e
Rchandrakanth <95973676+Rchandrakanth@users.noreply.github.com>
2025-08-07 02:30:05 +0530

    Update jenkins.sh

--- Jenkins/jenkins.sh
+++ Jenkins/jenkins.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install fontconfig git default-jre -y
+sudo apt install fontconfig git default-jre -y  
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
@@ -11,3 +11,18 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
 sudo apt-get update
 sudo apt-get install jenkins -y
 systemctl is-active jenkins
+
+============================================
+
+ - sudo chmod u+x jenkins.sh
+ - sudo ./jenkins.sh
+
+ - install docker commands also in jenkins vm  --> from the official page  or from  docker scripts 
+ 
+ - sudo docker chmod 666 /var/run/docker.sock
+
+ - open browser enter publicip: 8080   
+
+  -  sudo cat /var/lib/jenkins/secrets/initialAdminPassword   --> for password 
+  
+
